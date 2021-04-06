ab39a37e1ef1e3c41c2374ad798ae74f642c9071
adons1 <ant.adons@gmail.com>
2021-04-06 13:45:05 +0300

    print to file

--- IDEA_Cryptoalgorithm/printers.h
+++ IDEA_Cryptoalgorithm/printers.h
@@ -1,6 +1,5 @@
 wchar_t** allocate2Darray(int8_t, int8_t);
 void deallocate2Darray(wchar_t**, int8_t);
 void printWstringAs_UINT16_T(std::wstring, int8_t, int8_t, int8_t);
-void printWstringAs_UINT16_T(wchar_t*, int8_t, int8_t, int8_t);
 void print2Darray(wchar_t**, int8_t, int8_t, int8_t);
 std::wstring inHex(int);
